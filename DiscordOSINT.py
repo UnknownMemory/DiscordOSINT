ec997b0362b363deb2f56231592e018c321959e8
Jérémy <jeremyschneider21@gmail.com>
2021-02-01 18:17:47 +0100

    Add method to get the friends list

--- DiscordOSINT.py
+++ DiscordOSINT.py
@@ -2,6 +2,7 @@ import requests
 import base64
 import json
 
+
 class DiscordOSINT:
     def __init__(self, email, password):
         self.base_url = "https://discord.com/api/v8"
@@ -11,13 +12,17 @@ class DiscordOSINT:
         self.super_properties = None
         self.token = None
 
+        self.friends = None
+
         self.__get_token(email, password)
+
 
     def __get_fingerprint(self):
         # https://discord.com/api/v8/experiments give the X-Fingerprint if it doesn't exist in the headers of the request
         r = requests.get(f"{self.base_url}/experiments", headers={'User-Agent': self.user_agent})
         self.fingerprint = r.json().get('fingerprint')
         return self.fingerprint
+
     
     def __get_super_properties(self):
         # Create X-Super-Properties
@@ -37,9 +42,8 @@ class DiscordOSINT:
     
         b64_sp = base64.b64encode((str(super_properties).encode()))
         self.super_properties = b64_sp.decode()
-        
         return self.super_properties
-        
+
 
     def __get_token(self, email, password):
         # Connect to discord to get the authorization token
@@ -53,9 +57,22 @@ class DiscordOSINT:
                     "login_source": None,
                     "gift_code_sku_id":None}
 
-        r = requests.post(f"{self.base_url}/auth/login", 
-                          headers={"User-Agent": self.user_agent, "Content-Type": "application/json", "X-Fingerprint": self.fingerprint, "X-Super-Properties": self.super_properties},
-                          json=payload)
+        headers = {"User-Agent": self.user_agent,
+                   "Content-Type": "application/json",
+                   "X-Fingerprint": self.fingerprint,
+                   "X-Super-Properties": self.super_properties}
+
+        r = requests.post(f"{self.base_url}/auth/login", headers=headers, json=payload)
         self.token = r.json().get("token")
         return self.token
 
+    
+    def get_friends(self):
+        headers = {"User-Agent": self.user_agent, "Authorization": self.token, "X-Super-Properties": self.super_properties}
+        res = requests.get(f"{self.base_url}/users/@me/relationships", headers=headers)
+        
+        if res.status_code == 200:
+            self.friends = json.loads(res.text)
+            return self.friends
+
+        
